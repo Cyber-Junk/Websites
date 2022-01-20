92dc68d33cb6761b7520ae71364a5ab9aa621dcd
Harsh2220 <harshgajjar145@gmail.com>
2022-01-20 12:10:49 +0530

    Sponsor Card link added

--- src/Sponsors/SponsCard.js
+++ src/Sponsors/SponsCard.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-function SponsCard({ id, link, post }) {
+function SponsCard({ id, src, post }) {
     return (
         <div className="d-flex flex-column m-3 justify-content-between align-items-center p-3 bg-white shadow" style={{ borderRadius: "10px" }}>
-            <img src={require(`../Data/Sponsimage/${id}.webp`).default} className="" alt="Company_logo" height="150" width="150" />
+            {!src ?
+                <img src={require(`../Data/Sponsimage/${id}.webp`).default} className="" alt="Company_logo" height="150" width="150" />
+                :
+                <a href={src} style={{ cursor: "pointer" }} target="_blank">
+                    <img src={require(`../Data/Sponsimage/${id}.webp`).default} className="" alt="Company_logo" height="150" width="150" />
+                </a>
+            }
             <h6 className="mt-3">{post}</h6>
         </div>
     )
--- src/Team/Card.js
+++ src/Team/Card.js
@@ -5,7 +5,7 @@ function Card({ profile, name, post, linkedin, twitter }) {
     return (
         <div className="col-xl-3 col-sm-6 my-5 px-4">
             <div className="rounded-xl shadow-sm py-3 mycard">
-                <img src={profile} alt="Profile-Photo" className="img-fluid rounded-circle mb-3 shadow-sm" width="50%"/>
+                <img src={profile} alt="profile" className="img-fluid rounded-circle mb-3 shadow-sm" width="50%"/>
                 <h5 className="mb-0">{name}</h5><span className="small text-uppercase">{post}</span>
                 <ul className="social mb-0 list-inline mt-3">
                     <li className="list-inline-item"><a href={linkedin} className="social-link" target="_blank"><i className='bx bxl-linkedin'></i></a></li>

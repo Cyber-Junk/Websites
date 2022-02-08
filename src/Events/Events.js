fa9dc88280e9aef7939398a8095715598715d007
Harsh2220 <harshgajjar145@gmail.com>
2022-02-08 19:54:12 +0530

    event added

--- src/Events/Events.js
+++ src/Events/Events.js
@@ -4,13 +4,12 @@ import data from "../Data/events.json"
 
 function Events() {
     return (
-        <div className="container-fluid min-vh-100 text-center">
-            {/* <div className="row m-auto">
+        <div className="container-fluid min-vh-100">
+            <div className="row m-auto">
                 {data.map(el => {
-                    return <Event url={require(`../Data/posters/${el.id}.png`).default} name={el.name} detail={el.detail} link={el.link} />
+                    return <Event url={require(`../Data/posters/${el.id}.jpeg`).default} name={el.name} detail={el.detail} link={el.link} />
                 })}
-            </div> */}
-            <h1 className="coming">Coming Soon...</h1>
+            </div>
         </div>
     )
 }
